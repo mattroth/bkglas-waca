4f655c979bbc288bd2239502ff204926e42f12f7
mattroth <me@mattroth.com>
2024-04-15 11:18:42 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ var app       = express();
 // input port objects for our example
 var inputs = [
     { pin: '26', gpio: '7', value: null },
-    { pin: '5', gpio: '3', value: null },
-    { pin: '15', gpio: '22', value: null },
-    { pin: '22', gpio: '25', value: null },
+    //{ pin: '5', gpio: '3', value: null },
+    //{ pin: '15', gpio: '22', value: null },
+    //{ pin: '22', gpio: '25', value: null },
 ];
 
 // -----------------------------------------------------------------------
@@ -41,17 +41,17 @@ setInterval( function () {
 
     /* On for 2 second */
     rpio.write(inputs[0].pin, rpio.LOW);
-    rpio.write(inputs[1].pin, rpio.HIGH);
-    rpio.write(inputs[2].pin, rpio.LOW);
-    rpio.write(inputs[3].pin, rpio.HIGH);
+    //rpio.write(inputs[1].pin, rpio.HIGH);
+    //rpio.write(inputs[2].pin, rpio.LOW);
+    //rpio.write(inputs[3].pin, rpio.HIGH);
 
     rpio.sleep(2);
 
     /* Off for 1 second (1000ms) */
     rpio.write(inputs[0].pin, rpio.HIGH);
-    rpio.write(inputs[1].pin, rpio.LOW);
-    rpio.write(inputs[2].pin, rpio.HIGH);
-    rpio.write(inputs[3].pin, rpio.LOW);
+    //rpio.write(inputs[1].pin, rpio.LOW);
+    //rpio.write(inputs[2].pin, rpio.HIGH);
+    //rpio.write(inputs[3].pin, rpio.LOW);
 
     rpio.sleep(1);
 }, 3000); // setInterval
