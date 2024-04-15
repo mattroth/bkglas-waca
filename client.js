65840f84f5a5de091409106f2dadc8a846b152c4
mattroth <me@mattroth.com>
2024-04-15 12:37:00 -0700

    Update client.js

--- client.js
+++ client.js
@@ -9,26 +9,4 @@ window.onload = function () {
     } );
 
 
-    /**
-    var url,
-        i,
-        ports = [23, 25];  // the GPIO ports we will read
-
-    for (i in ports) {
-        $('#input_' + ports[i]).html('loading port ' + ports[i] + ' value...');
-    }
-
-    setInterval( function () {
-        for (i in ports) {
-            url = document.URL + 'inputs/' + ports[i];
-            console.log('making API call ' + url);
-
-            $.getJSON(url, function (data) {
-                console.log('API response received. port ' + data.gpio + ' value = ' + data.value);
-                $('#input_' + data.gpio).html('GPIO input port ' + data.gpio + ' value is ' + data.value);
-            });
-        } // for
-    }, 1000); // setInterval
-     **/
-
 }; //onload
